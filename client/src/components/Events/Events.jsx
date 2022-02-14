d2279871686f9e1506ef2c70fb9bd1e753f4a25b
“firas <server.scmp@gmail.com>
2022-02-14 10:30:22 +0200

    first commit

---
+++ client/src/components/Events/Events.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import stylesEvents from "../events.module.css";
+import Axios from 'axios'
+import stylesDepPage from "../Departments.module.css";
+import { useEffect, useState } from "react";
+import TopTitle from "../topTitle";
+import EventsCard from "./EventsCard";
+import { Link } from "react-router-dom";
+import Spinner from "../ClipLoader/Spinner";
+export default function Events(props) {
+  const [EventsLists, setEventsList] = useState([])
+  const [Loaded, setLoaded] = useState(false);
+ /*  const [startDate, setStartDate]=useState(new Date()); */
+  useEffect(() => {
+  Axios.get("http://test.scmp-lb.com:3002/api/Events").then((response)=>{
+    setLoaded(true)
+    setEventsList(response.data);
+  
+  
+  }
+  
+  ).catch(err=>{
+   
+  })
+      
+  }, [])
+
+  return ( <div className={stylesEvents.root}>
+    { Loaded ?  
+      <div>
+      <TopTitle title={"Events"}/>
+  <Row className={`g-4 ${stylesDepPage.root}`}>
+
+    {EventsLists.length>0? EventsLists.map((val)=>{return <Col key={val.EID} xs={12} sm={12}md={6}>
+   
+        <EventsCard  className={stylesDepPage.Col} date={val.EDate} Photos={val.EPhotos} paragraph={val.EParagraph} title={val.ETitle} text2={val.EParagraph} src={val.EID}  img={`/events/${val.ECover} `}  />
+      </Col>})  
+:<div></div>} 
+      </Row>
+   </div>
+ 
+    :<Spinner/>}
+ </div>
+  );
+}
---
+++ client/src/components/seperateEvent/seperateEvent.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Events from "../Events/Events";
+import EventsComp from "../EventsComp";
+import NotFound from "../NotFound";
+import Topstyles from "./seperateEvent.module.css";
+import { css } from "@emotion/react";
+
+import Spinner from "../ClipLoader/Spinner";
+export default function SeperateEvent(props) {
+  const [Event, setEvent] = useState([]);
+  const [Loaded,setLoaded]=useState(false)
+  const [errorFetchedChecker, seterrorFetchedChecker] = useState(false);
+  let { id } = useParams();
+
+   useEffect(() => {
+    async function fetchData(){
+    await axios
+      .post("http://test.scmp-lb.com:3002/api/SeperateEvent", { EID: id })
+      .then((response) => {
+  
+    if(response.data==="not found"){
+      setLoaded(true)
+        setEvent("");
+     
+      }
+    else if(response.data.length>0){
+      setLoaded(true)
+      setEvent(response.data)
+    }})
+    .catch(err=>{
+     
+      seterrorFetchedChecker((c) => !c);
+    });
+  }
+  if(errorFetchedChecker){
+    setTimeout(()=>{
+        fetchData();
+    },[3000])// 3 seconds
+} else {
+    fetchData();
+} }, [errorFetchedChecker]); 
+  return (
+    <div>
+
+   {Loaded? (Event.length>0 ? <div> <div className={Topstyles.titleEng}>
+        {" "}
+        <p className={Topstyles.date}>Date: {props.date||Event[0].EDate}</p>
+        <h1 className={Topstyles.title}>{props.title||Event[0].ETitle}</h1>
+        <p>{props.text2||Event[0].EBrief}</p>
+      </div>
+      <EventsComp img={props.img||Event[0].EPhotos} /></div>:  <NotFound/>) :<Spinner/> }
+    
+     
+    </div>
+  );
+}
