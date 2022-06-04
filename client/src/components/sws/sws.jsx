7b3d66f60b54d88bea7ef7a6c46525143a024663
Firas Rihan <server.scmp@gmail.com>
2022-06-04 13:37:36 +0300

    sws-results

--- client/src/components/sws/sws.jsx
+++ client/src/components/sws/sws.jsx
@@ -50,11 +50,45 @@ export default function Sws() {
 
 
     <div className={styleSws.root}>
-      
     <div className={styleSws.swsCarousel}><CarouselScmp style={20} img={Imgs} /></div>
-    <h2 className={styleSws.header}>The Scientific Center for Manufacturing and Production announces the organization of its first Startup Weekend Support! <br /> SCMP-SWS2022</h2>
+    <h2 className={styleSws.header}>SCMP congratulates all participants in SWS2022!</h2>
 
-      {/* {Loaded? */}
+  
+    <p className={styleSws.firstParagraph}>  
+    Greetings candidates!
+    <br/><br/>
+    <p className={styleSws.Indent}>
+First of all, we would like to thank each participant that have joined us in our first Startup Weekend Support SWS-2022 event which was held on the 28th of May in cooperation with the Lebanese University. 
+</p>
+As you know, the number of applicants for this event has reached 76 competing teams, from whom we preselected 37 different ideas from various fields (Health, Agro-Food, Education, Technology and E-Business). 
+
+After presenting these ideas in front of specialized committees composed of academics, experts and businessmen, it became clear to us that all the ideas have potential, but since we have to choose a finite number of ideas, these 37 chosen ideas were further filtered out and sorted according to the priorities that the Scientifc Center for Manufacturing and Production is looking for in addition to the consultation of the evaluation committee which led us to choose the following 14 ideas listed alphabetically, these 14 ideas will be proceeding to the next stage:
+<br/><br/>
+<ol>
+<li>3dLife </li>
+<li>A Little goes a Long Way</li>
+<li>Carrot Pulp Chips </li>
+<li>Dental Medical Products</li>
+<li>Edubolt</li>
+<li>Educational Platform</li>
+<li>Health and Diet</li>
+<li>Khadamat</li>
+<li>Makit</li>
+<li>Multi-Vitamine Immune Boosting Lollipops </li>
+<li>PP Bread</li>
+<li>Shopex</li>
+<li>Teeth Tech</li>
+<li>Un Petit Goût</li>
+</ol>
+<br/>
+The above-mentioned ideas that qualified for the semi-finals will be notified later on regarding the next steps.
+
+As for the ideas that were not selected, they will remain with us in a list of ideas that will be presented to different businessmen and financiers in a frequent manner in hopes of finding someone interested in incubating them in order to grant you further opportunities and help you grow.
+<br/>
+</p>
+{/*    
+  
+  
       <p className={styleSws.Application}> To Apply for this Event, Please Fill the Following  <a href="https://forms.gle/6cabDREDmKrLm4hm7" className={styleSws.formLink} >Application</a> before midnight of <span className={styleSws.oldDate}>6-May-2022</span></p>
    
       <h2 className={styleSws.newDate}>In response to some requests, the deadline of ideas submission has been extended until midnight of 15-May-2022!</h2>
@@ -76,7 +110,7 @@ export default function Sws() {
           <li>Legal</li>
           <li>Logistical</li>
         </ul>  <br /> Assistance to launch new Startups. <br /> <br />
-        Another objective of this Weekend is benefiting from technology to stimulate innovation, to market ideas, and to promote cooperation between different stake holders. {/* responsible for the success of the proposals. */}
+        Another objective of this Weekend is benefiting from technology to stimulate innovation, to market ideas, and to promote cooperation between different stake holders.
         The competitive participation is open to ideas with technological background in the following fields:
         <br />
         <br />
@@ -114,7 +148,7 @@ export default function Sws() {
 
       </div>
       <div className={styleSws.applyBtnDiv}> <Button onClick={() => { window.location.href = "https://forms.gle/6cabDREDmKrLm4hm7" }} className={styleSws.applyBtn} variant='warning' >Apply Here!</Button></div>
-    </div></div> : <div><Spinner /></div>} </div>
+    </div> */}</div> </div> : <div><Spinner /></div>} </div>
 
 
 
