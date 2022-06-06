37b4ac1b528e8a982a8ca1bb5636f3cb5e5ea69d
FirasRihanSCMP <98955064+FirasRihanSCMP@users.noreply.github.com>
2022-06-06 15:46:00 +0300

    new-sws-images

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
   useEffect(() => {
     async function fetchData() {
       await axios
-        .get("https://www.scmp-lb.com/api/home")
+        .get("http://192.168.0.140:3002/api/home")
         .then((response) => {
 
           if (response.data === "not found") {
@@ -46,7 +46,8 @@ export default function Home() {
   return (
     <div>
       {Loaded ? <div className={stylesHome.root} >
-        <CarouselScmp img={Imgs} style={24} />
+        <div className={stylesHome.carouselContainer}>   <CarouselScmp img={Imgs} style={24} /></div>
+
         <Paragraph img={"./imgs/planner.webp"} topTitle={"Our Mission"} text={"SCMP works on studying needs as inputs, prioritizing them, and working within a specific time plan to produce studies and spatial planning which includes three items: standing situation, desired situation, and the proposed roadmap. Three types of outputs emerge from this spatial planning. First, the feasibility studies that turn into productive projects based on the partnership of three parties (the local capital, the center that provides all the required scientific services, the source of technology). Second, investment priorities according to which SCMP will enlarge existing small businesses. Last, new innovative ideas that are being used in a scientific, technical and financial greenhouse towards emerging companies."} />
         <AboutUsTop />
         <Firstcards title={["Technology Broker", "Energy and Water", "Food Industry"]} text={["We use our knowledge, skills, and resources to increase the productive capacity of the economy.", "We offer innovative and sustainable solutions for the production and consumption of energy and usage of water.", "Our vision in the Food Industry department is to be a leading provider of services, researchs and consultancies on agri-food sector."]} img={["techcover.webp", "energycover.webp", "photo1.webp"]} src={["Technology", "Energy", "Food"]} />
